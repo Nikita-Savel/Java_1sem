8570be1f1420a62428e6de7062103d84da5fcac4
Nikita-Savel <n89227567430@yandex.ru>
2023-12-11 22:02:12 +0300

    rm all

--- FilterOfComments/Label.java
+++
@@ -1,8 +0,0 @@
-package FilterOfComments;
-
-public enum Label {
-    OK,
-    SPAM,
-    NEGATIVE_TEXT,
-    TOO_LONG
-}
--- Test6/Test6.java
+++
@@ -1,9 +0,0 @@
-package Test6;
-public class Test6 {
-    public static void main(String[] args) {
-        Contact myIvan = new Contact("Иван", "ivan@gmail.com");
-        myIvan.sayHello();
-        Contact myAngelina = new Contact("Ангелина", "angelina@yandex.ru");
-        myAngelina.sayHello();
-    }
-}
